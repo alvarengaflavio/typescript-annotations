70381b824591f60236ca896197390ef3f322959f
alvalenda <banysan@msn.com>
2022-10-23 16:18:41 -0300

    +intersection types

---
+++ Types/5-intersectionTypes.ts
@@ -0,0 +1,21 @@
+/*
+    Intersection Types
+    Intersection types are a way to combine multiple types into one.
+    Intersection types are declared using the ampersand symbol (&).
+    Intersection types are declared outside of the function body.
+*/
+
+type Circle = {
+  radius: number;
+};
+
+type Colorful = {
+  color: string;
+};
+
+type CircleWithColor = Circle & Colorful;
+
+const happyFace: CircleWithColor = {
+  radius: 10,
+  color: "yellow",
+};
