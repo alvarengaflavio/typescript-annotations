4c4a4906d623ca55fa5130c3e3b1acb606faff5e
alvalenda <banysan@msn.com>
2022-10-25 09:08:40 -0300

    MINIPROJECT: part 8

--- MINIPROJECT/dist/index.js
+++ MINIPROJECT/dist/index.js
@@ -2,8 +2,13 @@
 // read the README for learn about typescript and the DOM types.
 const btn = document.querySelector("#btn"); // Type: HTMLButtonElement
 const input = document.querySelector("#todoinput"); // Type: HTMLInputElement
+const form = document.querySelector("form"); // Type: HTMLFormElement
 // <HTML...> this assertion does not work with jsx element, in this case we need to use the "as" keyword to cast the type.
-btn.addEventListener("click", () => {
-    alert(input.value);
-    input.value = "";
-}); // no error
+// btn.addEventListener("click", () => {
+//   alert(input.value);
+//   input.value = "";
+// }); // no error
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(input.value);
+});
--- MINIPROJECT/src/index.ts
+++ MINIPROJECT/src/index.ts
@@ -2,10 +2,16 @@
 
 const btn = <HTMLButtonElement>document.querySelector("#btn"); // Type: HTMLButtonElement
 const input = document.querySelector("#todoinput") as HTMLInputElement; // Type: HTMLInputElement
+const form = document.querySelector("form")!; // Type: HTMLFormElement
 
 // <HTML...> this assertion does not work with jsx element, in this case we need to use the "as" keyword to cast the type.
 
-btn.addEventListener("click", () => {
-  alert(input.value);
-  input.value = "";
-}); // no error
+// btn.addEventListener("click", () => {
+//   alert(input.value);
+//   input.value = "";
+// }); // no error
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(input.value);
+});
