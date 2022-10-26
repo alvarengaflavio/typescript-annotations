17e2a0b2bf15b6d4be8e4dd0224cb006a202695f
alvalenda <banysan@msn.com>
2022-10-26 17:44:19 -0300

    Inheritance, Polymorphism and super()

--- Classes/src/javascriptClasses.js
+++ Classes/src/javascriptClasses.js
@@ -1,16 +1,15 @@
-/*
-                        Javascript Classes
+/*  -----------------------------------------------------------------------------------------------------
+                                        Javascript Classes
 
-    Classes are templates for creating objects in JavaScript. They contain a few different important pieces which allow us to create objects with similar properties and methods.
+    -   Classes are templates for creating objects in JavaScript. They contain a few different important 
+        pieces which allow us to create objects with similar properties and methods.
+    -   Classes can be extended to create subclasses. Subclasses can inherit properties and methods from 
+        their parent class.
+    -   Classes can also be used to create objects using the new keyword. This is called instantiation.
+    -   Classes are a great way to organize code and create reusable templates for objects.
+    -   Classes are a tool that developers use to quickly produce similar objects.
 
-    Classes can be extended to create subclasses. Subclasses can inherit properties and methods from their parent class.
-
-    Classes can also be used to create objects using the new keyword. This is called instantiation.
-
-    Classes are a great way to organize code and create reusable templates for objects.
-
-    Classes are a tool that developers use to quickly produce similar objects.
-*/
+    ----------------------------------------------------------------------------------------------------- */
 
 // Create a class using the class keyword
 class Player {
@@ -32,12 +31,14 @@ player1.introduce(); // Hi I am Ramon, I'm a Warrior
 const player2 = new Player("Fafa", "Bard"); // Name: Fafa Type: Bard
 player2.introduce(); // Hi I am Fafa, I'm a Bard
 
-/*
-            Class Fields
+/*  -----------------------------------------------------------------------------------------------------
+                                        Class Fields
 
-    -   Class fields are a new feature in JavaScript that allow us to add properties directly to the class. This is a great way to add properties to a class that are not shared by all instances of the class.
+    -   Class fields are a new feature in JavaScript that allow us to add properties directly to the class. 
+    -   This is a great way to add properties to a class that are not shared by all instances of the class.
     -   Class fields are added to the class body, not the constructor method.
-*/
+
+    ----------------------------------------------------------------------------------------------------- */
 
 class Player {
   // if all players start with the same score and lives, we can add them to the class body
@@ -66,13 +67,15 @@ console.log(player3); // Player {name: "ChikenFace", type: "Healer", score: 0, 
 player3.loseLife(); // ChikenFace lost a life
 console.log(player3); // Player {name: "ChikenFace", type: "Healer", score: 0, lives: 2}
 
-/*
-            private properties
+/*  -----------------------------------------------------------------------------------------------------
+                                        Private properties
     
-    -   Private properties are a new feature in JavaScript that allow us to create private variables for a class. Private properties begin with a # symbol.
+    -   Private properties are a new feature in JavaScript that allow us to create private variables for a class.
+    -   Private properties begin with a # symbol.
     -   Private properties are not accessible outside of the class.
     -   Private properties are useful for data that is not shared by all instances of the class.
-*/
+
+    ----------------------------------------------------------------------------------------------------- */
 
 class Player {
   #score = 0; // Private property
@@ -112,13 +115,15 @@ console.log(player4.getScore()); // 100
 
 // We can also add private methods to a class. This allows us to create methods that are not accessible outside of the class. Private methods begin with a # symbol. Like #gameOver() in the example above.
 
-/*
-            Getters and Setters
+/*  -----------------------------------------------------------------------------------------------------
+                                            Getters and Setters
 
-    -   Getters and setters are methods that get and set the properties of an object. They are used to control access to an object.
+    -   Getters and setters are methods that get and set the properties of an object. They are used to control
+        access to an object.
     -   Getters and setters are useful for performing actions on properties when they are accessed or set.
     -   Getters and setters are defined using the get and set keywords.
-*/
+
+    ----------------------------------------------------------------------------------------------------- */
 
 class Player {
   #life = 3;
@@ -159,13 +164,15 @@ player5.loseLife(2); // Moirinas lost 2 life
 console.log(player5); // Player {name: "Moirinas", type: "Archer", #life: 1}
 player5.loseLife(2); // Game Over!
 
-/*
-            Static Fields and Methods
+/*  -----------------------------------------------------------------------------------------------------
+                                        Static Fields and Methods
 
-    -   Static fields and methods are properties and methods that are attached to the class itself, not the instances of the class.
+    -   Static fields and methods are properties and methods that are attached to the class itself, not the 
+        instances of the class.
     -   Static fields and methods are useful for creating utility functions for a class.
     -   Static fields and methods are defined using the static keyword.
-*/
+    
+    ----------------------------------------------------------------------------------------------------- */
 
 class Player {
   static maxLife = 3; // Static field
@@ -214,11 +221,74 @@ player6.changeLife(5); // Arlindo Cross gained "5" life
 console.log(player6); // Player {name: "Arlindo Cross", type: "Paladin", #life: 3}
 player6.changeLife(-3); // Game Over!
 
-/*
-            Inheritance
+/*  -----------------------------------------------------------------------------------------------------
+                                                Inheritance
 
     -   Inheritance is a way to create a new class from an existing class.
     -   The new class is called a subclass, and the existing class is called the superclass.
     -   The subclass inherits all of the properties and methods of the superclass.
     -   We can use the extends keyword to create a subclass.
-*/
+
+    ----------------------------------------------------------------------------------------------------- */
+
+class ModeratorPlayer extends Player {
+  // We use the extends keyword to create a subclass
+  constructor(name, type) {
+    super(name, type); // We use the super keyword to call the constructor of the superclass
+  }
+
+  mute(player) {
+    player.muted = true;
+    console.log(`${player.name} is muted.`);
+  } // We can also add new methods to the subclass
+}
+
+const moderator = new ModeratorPlayer("Biggus Dickus", "Loman");
+moderator.introduce(); // Hi I am Biggus Dickus, I'm a Loman
+console.log(moderator); // Player {name: "Biggus Dickus", type: "Loman", #life: 3}
+moderator.mute(player6); // Arlindo Cross is muted.
+
+/*  -----------------------------------------------------------------------------------------------------
+                                            Polymorphism
+
+    -   Polymorphism is the ability to call the same method on different objects and get different results.
+    -   Polymorphism is useful for creating reusable code.
+    -   Polymorphism is achieved by overriding methods in subclasses.
+
+    ----------------------------------------------------------------------------------------------------- */
+
+class ModeratorPlayer extends Player {
+  #life = 9;
+  constructor(name, type) {
+    super(name, type);
+    powers = ["mute", "kick", "ban"];
+  }
+
+  introduce() {
+    console.log(
+      `Hi I am ${this.name}, I'm a ${this.type} and I have the powers ${this.powers}`
+    );
+  } // We can override methods in subclasses
+}
+
+const moderator2 = new ModeratorPlayer("Incontinentia Buttocks", "Wife");
+moderator2.introduce(); // Hi I am Incontinentia Buttocks, I'm a Wife and I have the powers mute,kick,ban
+console.log(moderator2); // Player {name: "Incontinentia Buttocks", type: "Wife", #life: 9}
+
+/*  -----------------------------------------------------------------------------------------------------
+                                           Encapsulation
+
+    -   Encapsulation is the process of hiding the implementation details of a class from other objects.
+    -   Encapsulation is achieved by using private fields and methods.
+    -   Encapsulation is useful for creating reusable code.
+
+    ----------------------------------------------------------------------------------------------------- */
+
+/*  -----------------------------------------------------------------------------------------------------
+                                            Abstraction
+
+    -   Abstraction is the process of hiding the implementation details of a class from other objects.
+    -   Abstraction is achieved by using private fields and methods.
+    -   Abstraction is useful for creating reusable code.
+
+    ----------------------------------------------------------------------------------------------------- */
