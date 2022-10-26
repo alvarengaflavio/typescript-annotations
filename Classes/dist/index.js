351a32718b3d3b980a141584637768a067f0b03a
alvalenda <banysan@msn.com>
2022-10-26 17:50:59 -0300

    Inheritance, Polymorphism and super()

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
+/*  -----------------------------------------------------------------------------------------------------
+                                           TypeScript Classes
+
+    -   Classes in TypeScript are similar to classes in other languages like Java or C#.
+    -   TypeScript supports new features in JavaScript, like support for class-based object-oriented
+        programming.
+    -   Classes in TypeScript support inheritance, constructors, getters/setters, and static members.
+    -   Classes are in fact "special functions", and just as you can define functions, you can also define
+        classes.
+    -   Classes are a way to create objects with pre-defined properties and methods.
+    -   Classes encapsulate data with code to work on that data.
+    -   Classes are a way to define a custom type.
+
+    ----------------------------------------------------------------------------------------------------- */
 console.log("Hello World");
