7f5574b70325ed4c72909fe551fa4de78536abfe
alvalenda <banysan@msn.com>
2022-11-02 19:35:01 -0300

    Truthiness Guards

--- TypeofGuards/index.ts
+++ TypeofGuards/index.ts
@@ -1,4 +1,4 @@
-/* ------------------------------------------------------------------------------- 
+/* ------------------------------------------------------------------------------------- 
                                     TypeofGuards    
 
     -   This file contains a number of functions that can be used to determine the
@@ -9,7 +9,7 @@
     TypeScript's type system.  They are meant to be used as a last resort when
     you need to determine the type of a variable at runtime.  
 
-    ------------------------------------------------------------------------------- */
+    ------------------------------------------------------------------------------------- */
 
 function tripleThings(x: number | string): number | string | void {
   if (typeof x === "number") return x * 3; // x has been narrowed to number
@@ -24,3 +24,40 @@ const y = "5";
 
 console.log(tripleThings(x)); // x is a number, so the function will return a number (15)
 console.log(tripleThings(y)); // y is a string, so the function will return a string ("555")
+
+/* -------------------------------------------------------------------------------------
+                                    Truthiness Guards
+
+    -   Truthiness Type Guards involve checking a value for being truthy or falsy before 
+    narrowing the type of a variable.  This is useful when you need to avoid erros when 
+    values might be null or undefined.
+    -   We do this all the time with vanilla JavaScript. But here we are just showing how
+    to do it with TypeScript.
+
+  -------------------------------------------------------------------------------------- */
+
+// for exempla in a html document get by id, the type can be HTMLElement or null
+// so we can use the truthiness guard to check if the element is null or not
+const element = document.getElementById("myElement");
+if (element) {
+  // element is not null
+  element.innerHTML = "Hello World"; // thats it. Here is HTMLElement
+  // This is the truthiness guard to check if a value is null or undefined.
+} else {
+  // element is null and TypeScript knows it
+  console.log(element, "is null"); // element is null
+}
+
+// ANOTHER EXAMPLE
+const printLetters = (word?: string) => {
+  // word?: string == word: string | undefined
+  if (word) {
+    // word is not undefined in this scope
+    for (let letter of word) {
+      console.log(letter);
+    }
+  } else {
+    // word is undefined or a "false string" in this scope
+    console.log("You did not pass in a word");
+  }
+}; // This function will print each letter of a string to the console.
