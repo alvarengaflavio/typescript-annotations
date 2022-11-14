bf19e2dbcedcf6e68e42e037ae7522715aebace4
alvalenda <banysan@msn.com>
2022-11-14 07:15:39 -0300

    DECORATORS AS CONSTRUCTORS

--- Configuration/dist/index.js
+++ Configuration/dist/index.js
@@ -29,3 +29,33 @@ const norma = {
 // The "outDir" property specifies the output folder for the compiled files.
 // The "rootDir" property specifies the root folder for the source files.
 // check the tsconfig.json file in this folder to see the configuration. There i added the "outDir" and "rootDir" properties.
+/*  ------------------------------------------------------------------------------------------------
+                                          target option
+    ------------------------------------------------------------------------------------------------ */
+// The "target" property specifies the version of JavaScript that the compiler will use to compile the code.
+// Basic options: "es3", "es5", "es6", "es2015", "es2016", "es2017", "es2018", "es2019", "es2020", "esnext".
+// The "es5" option will use the version of JavaScript that was used in the first version of the ECMAScript standard.
+// The "es6" option will use the version of JavaScript that was used in the second version of the ECMAScript standard. This is the same as "es2015". This is the default option. This is a good option to use because it is compatible with most browsers and uses the latest features of JavaScript. Like the "let" and "const" keywords, arrow functions, classes, etc.
+// The "esnext" option will use the latest version of JavaScript.
+// check the tsconfig.json file in this folder to see the configuration. There i added the "target" property.
+/*  ------------------------------------------------------------------------------------------------
+                                          strict option
+    ------------------------------------------------------------------------------------------------ */
+// The "strict" property is a boolean option that enables all strict type checking options.
+// The "strict" property is a good option to use because it will help you to avoid errors in your code.
+// The "strict" property is the same as the following options:
+// "noImplicitAny": true,
+// "strictNullChecks": true,
+// "strictFunctionTypes": true,
+// "strictBindCallApply": true,
+// "strictPropertyInitialization": true,
+// "noImplicitThis": true,
+// "alwaysStrict": true,
+// check the tsconfig.json file in this folder to see the configuration. There i added the "strict" property.
+/*  ------------------------------------------------------------------------------------------------
+                                          module option
+    ------------------------------------------------------------------------------------------------ */
+// The "module" property specifies the module system that the compiler will use to compile the code.
+// Basic options: "commonjs", "amd", "system", "umd", "es6", "es2015", "es2020", "esnext".
+// The "commonjs" option will use the CommonJS module system.
+// for the sake of sanity, i'll not show all options here. Check the documentation for more information. https://www.typescriptlang.org/tsconfig
--- Decorators/dist/index.js
+++ Decorators/dist/index.js
@@ -1,5 +1,6 @@
+"use strict";
 /*  -------------------------------------------------------------------------------------
-                                  DECORATORS
+                                DECORATORS
 
     -   Decorators are functions that can be used to modify the behavior of a class, method,
     property, or parameter. Decorators are applied by placing them above the declaration
@@ -10,51 +11,47 @@
     not at runtime. Decorators are applied to the class constructor returned by the class
     declaration, not to the class declaration itself.
     ------------------------------------------------------------------------------------- */
-
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 // In the acutal version of TypeScript and JavaScript, decorators are an experimental
 // feature. To enable them, you must enable the experimentalDecorators compiler option
 // either on the command line or in your tsconfig.json.
-
 // Execute the following command line in the terminal:
 //  tsc --target ES5 --experimentalDecorators
-
 // Change the following configuration option in your config file:
 /*
     {
-	    "compilerOptions": {
-		    "target": "ES5",
-		    "experimentalDecorators": true
-	    }
+        "compilerOptions": {
+            "target": "ES5",
+            "experimentalDecorators": true
+        }
     }
 */
-
 // To use decorators, you must import the @types/core-decorators package from npm.
 // This package contains type definitions for the core-decorators library.
-
 /*  -------------------------------------------------------------------------------------
-                              DECORATORS AS CONSTRUCTORS 
+                                DECORATORS AS CONSTRUCTORS
                             
     -   Decorators are functions that are called at design time, when the class is defined,
     not at runtime. Decorators are applied to the class constructor returned by the class
     declaration, not to the class declaration itself.
     ------------------------------------------------------------------------------------- */
-
 // The following example shows how to use a decorator to modify the behavior of a class
 // constructor.
-
-function Log(constructor: Function) {
-  console.log(`New: ${constructor.name}`);
+function log(constructor) {
+    return function (target) {
+        console.log("New: ".concat(constructor.name));
+    };
 }
-
-@Log
-class Person {
-  constructor(public name: string) {}
-}
-
-const classObject = new Person("ChickenFace");
-
-// The following example shows how to use a decorator to modify the behavior of a class
-// constructor.
-// Whenever a new instance of the class is created, the decorator function is called. The
-// decorator function can then modify the behavior of the class constructor. In this case,
-// the decorator function logs proprety name of the class to the console.
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person = __decorate([
+        log()
+    ], Person);
+    return Person;
+}());
